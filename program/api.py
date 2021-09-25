71a6470e76ffd43df52d9d1933f9e11de4865bf8
落花有意12138 <luohuayouyi12138@outlook.com>
2021-09-25 15:42:08 +0800

    添加api程序

---
+++ program/api.py
@@ -0,0 +1,51 @@
+'''
+MIT License
+
+author: 
+落花有意12138(https://zh.wikisource.org/wiki/User:%E8%90%BD%E8%8A%B1%E6%9C%89%E6%84%8F12138)
+MediaWiki(https://mediawiki.org/)
+'''
+import requests
+
+S = requests.Session()
+apiUrl = "https://zh.wikisource.org/w/api.php"
+
+def getToken(type: str):
+    PARAMS = {
+        "action": "query",
+        "meta": "tokens",
+        "type": type,
+        "format": "json"
+    }
+    R = S.get(url=apiUrl, params=PARAMS)
+    DATA = R.json()
+    TOKEN = DATA['query']['tokens'][type+'token']
+
+    return TOKEN
+
+def login(name: str, password: str, token: str):
+    PARAMS = {
+        "action": "login",
+        "lgname": name,
+        "lgpassword": password,
+        "lgtoken": token,
+        "format": "json"
+    }
+    R = S.post(apiUrl, data=PARAMS)
+    return R
+
+def edit(title: str, text: str, token: str, summary: str="此次编辑为机器人编辑"):
+    PARAMS = {
+        "action": "edit",
+        "title": title,
+        "token": token,
+        "text": text,
+        "format": "json",
+        "bot": True,
+        # "createonly": True,#当存在时不要编辑页面
+        "summary": summary#编辑摘要
+    }
+    R = S.post(apiUrl, data=PARAMS)
+    DATA = R.json()
+
+    return DATA
